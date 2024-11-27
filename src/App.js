38e8efb34ca65b158e0b427913fe560a53d5257b
Lavanyasugumar96 <lavanyasugumar.slm@gmail.com>
2024-11-27 19:09:54 +0530

    created login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+const [username,setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [message, setMessage] = useState("");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if(!username || !password){
+    setMessage("Both fields for mandatory.");
+  }else if (username === "user" && password === "password") {
+    setMessage("Welcome, user!");
+  }else{
+    setMessage("Invalid username or password");
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className ="xlogin">
+  <form className = "xlogin-form" onSubmit = {handleSubmit} >
+    <div className = "xlogin-field">
+    <label htmlFor = "username">Username</label>
+    <input 
+    type = "text"
+    id = "username"
+    value = {username}
+    onChange = {(e) => setUsername(e.target.value)}
+    />
     </div>
+    <div className = "xlogin-field">
+      <label htmlFor = "password">Password</label>
+      <input
+      type = "text"
+      id="password"
+      value={password}
+      onChange = {(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type = "submit" className = "xlogin-button">
+        Submit
+      </button>
+  </form>
+  {message && (
+        <p className={`xlogin-message ${message === "Welcome, user!" ? "success" : "error"}`}>
+          {message}
+        </p>
+      )}
+</div>
   );
 }
 
